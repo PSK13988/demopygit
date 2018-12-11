7cf4a677122e965b83ba773bed6fc0620e1b70d3
psk13988 <pankajkharche13@gmail.com>
2018-12-11 16:48:47 +0530

    - Student.py added - demo.py modified

---
+++ Student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, college, age, gender):
+        self.name = name
+        self.college = college
+        self.age = age
+        self.gender = gender
--- demo.py
+++ demo.py
@@ -1,6 +1,11 @@
+from Student import Student
+
+
 # function for saying hello
 def say_hello(name):
     print("Hello Welcome, " + name)
 
 
-say_hello("Jim")
+student = Student("Mike", "NIIT", 30, "Male")
+
+say_hello(student.name)
